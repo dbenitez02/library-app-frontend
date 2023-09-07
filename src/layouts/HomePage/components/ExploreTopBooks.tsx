9489ae272cb66800fe04abd8a60f9dfe18bacc78
dbenitez02 <dbenitez101@gmail.com>
2023-09-06 21:17:13 -0700

    added router and linked all buttons

--- src/layouts/HomePage/components/ExploreTopBooks.tsx
+++ src/layouts/HomePage/components/ExploreTopBooks.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 /* eslint-disable jsx-a11y/anchor-is-valid */
 export const ExploreTopBooks = () => {
     return(
@@ -7,8 +9,8 @@ export const ExploreTopBooks = () => {
                 <div>
                     <h1 className="diaplay-5 fw-bold">Find your next adverture</h1>
                     <p className="col-md-8 fs-4">Where would you like to go next?</p>
-                    <a type="button" className="btn main-color btn-lg text-white" href="#">
-                        Explore top books</a>
+                    <Link type="button" className="btn main-color btn-lg text-white" to='/search'>
+                        Explore top books</Link>
                 </div>
             </div>
         </div>
