b54a526a9e9f0ecdc23ed1a54bc1b8792a913690
dbenitez02 <dbenitez101@gmail.com>
2023-09-10 20:46:25 -0700

    Added authroization to the home page

--- src/layouts/HomePage/components/Heros.tsx
+++ src/layouts/HomePage/components/Heros.tsx
@@ -1,4 +1,11 @@
+import { useOktaAuth } from "@okta/okta-react";
+import { Link } from "react-router-dom";
+
 export const Heros = () => {
+
+    const { authState } = useOktaAuth();
+
+    
     return (
         <div>
             <div className="d-none d-lg-block">
@@ -14,7 +21,11 @@ export const Heros = () => {
                                 Whether it is t learn a new skill or grow within one,
                                 we will be able to provide the top conent for you!
                             </p>
-                            <a className="btn main-color btn-lg text-white" href="#">Sign up</a>
+                            { authState?.isAuthenticated ?
+                                <Link type="button" className="btn main-color btn-lg text-white" to='search'>Explore top books</Link>
+                                :
+                                <Link className="btn main-color btn-lg text-white" to="/login">Sign up</Link>
+                            }
                         </div>
                     </div>
                 </div>
@@ -38,7 +49,7 @@ export const Heros = () => {
                 </div>
             </div>
 
-            { /* Moblie Heros*/}
+            { /* Moblie */}
             <div className="d-lg-none">
                 <div className="container">
                     <div className="m-2">
@@ -50,7 +61,11 @@ export const Heros = () => {
                                 Whether it is t learn a new skill or grow within one,
                                 we will be able to provide the top conent for you!
                             </p>
-                            <a className="btn main-color btn-lg text-white" href="#">Sign up</a>
+                            { authState?.isAuthenticated ?
+                                <Link type="button" className="btn main-color btn-lg text-white" to='search'>Explore top books</Link>
+                                :
+                                <Link className="btn main-color btn-lg text-white" to="/login">Sign up</Link>
+                            }
                         </div>
                     </div>
                     <div className="m-2">
--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,6 +1,6 @@
 export const oktaConfig = {
-    clientId: 'client-id',
-    issuer: "issuer",
+    clientId: '0oab6nfhlmCz6L12B5d7',
+    issuer: "https://dev-48810062.okta.com/oauth2/default",
     redirectUri: 'http://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
